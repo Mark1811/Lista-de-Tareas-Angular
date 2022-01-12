37a71cc8e8197edc7424c23ade66d00a3287e28b
Marcos Aguero <78196802+Mark1811@users.noreply.github.com>
2022-01-12 03:19:33 -0300

    Eliminaciones de carpetas

---
+++ src/app/agregar-comp/agregar-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-comp',
+  templateUrl: './agregar-comp.component.html',
+  styleUrls: ['./agregar-comp.component.css']
+})
+export class AgregarCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
